77971b0e7cfba0aee06ae88e5fa0a379d0522f07
Mateusz Olszewski <matcrono@gmail.com>
2020-12-07 11:26:04 +0100

    Complete Task 5.3

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let PlayerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove );
