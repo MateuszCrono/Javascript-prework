b2a0f5bc8d5cb3fcfaf6abfb7648f2526b33ae1b
Mateusz Olszewski <matcrono@gmail.com>
2020-12-07 13:41:55 +0100

    Incorrect Beta version of results

--- js/script.js
+++ js/script.js
@@ -29,3 +29,26 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}   else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Komputer wygrał!');
+}   else if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Mamy remis!');
+}   else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Mamy remis!');
+}   else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}   else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrał!');
+}   else if( computerMove == 'kamień' && playerMove == 'kamień'){
+        printMessage('Mamy remis!');
+}   else if( computerMove == 'nożyce' && playerMove == 'papier'){
+        printMessage('Komputer wygrał!');
+}   else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+        printMessage('Mamy remis!');
+}   else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+    else( playerMove == 'nieznany ruch'){
+        printMessage('Coś nie pykło');
